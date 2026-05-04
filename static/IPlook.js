3c5920e0f9cb09084998dd68159a80cd3db53a71
Hardik <hardikprajapati242008@gmail.com>
2026-05-04 12:09:34 +0530

    Fixed IP Look up

--- static/IPlook.js
+++ static/IPlook.js
@@ -1,11 +1,29 @@
 let btn = document.querySelector("#Getdata");
 let result = document.querySelector(".result");
-btn.addEventListener("click",async ()=>{
+
+btn.addEventListener("click", async () => {
     let ip = document.querySelector("#IP").value;
-    result.innerText = `Wait a min ${ip}`
-    url = `https://ip-api.com/json/${ip}`
-    response = await fetch(url);
-    data = await response.json()
-   
-    result.innerText = JSON.stringify(data,null,2);
+    
+    if (!ip) {
+        result.innerText = "Please enter an IP address";
+        return;
+    }
+
+    result.innerText = `Wait a min, fetching data for: ${ip}...`;
+
+    try {
+        // ip-api.com ki jagah ipapi.co use kar rahe hain
+        let url = `https://ipapi.co/${ip}/json/`;
+        let response = await fetch(url);
+        
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        let data = await response.json();
+        result.innerText = JSON.stringify(data, null, 2);
+    } catch (error) {
+        result.innerText = "Error: " + error.message + ". Try a different IP or check connection.";
+        console.error("Fetch error:", error);
+    }
 });
